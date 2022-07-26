a09c08f872087476c7f462c8e2b32fb3de0d6d27
Michael Nguyen <67665595+michaelnguyen26@users.noreply.github.com>
2022-07-26 15:21:28 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,51 @@
+# streamlit_app.py 
+
+ 
+
+import streamlit as st 
+
+import snowflake.connector 
+
+ 
+
+# Initialize connection. 
+
+# Uses st.experimental_singleton to only run once. 
+
+@st.experimental_singleton 
+
+def init_connection(): 
+
+    return snowflake.connector.connect(**st.secrets["snowflake"]) 
+
+ 
+
+conn = init_connection() 
+
+ 
+
+# Perform query. 
+
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min. 
+
+@st.experimental_memo(ttl=600) 
+
+def run_query(query): 
+
+    with conn.cursor() as cur: 
+
+        cur.execute(query) 
+
+        return cur.fetchall() 
+
+ 
+
+rows = run_query("SELECT * from mytable;") 
+
+ 
+
+# Print results. 
+
+for row in rows: 
+
+    st.write(f"{row[0]} has a :{row[1]}:")
