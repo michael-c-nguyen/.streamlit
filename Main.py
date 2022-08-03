4d061b8387230c6a60da1533f1a8e94519da2731
Michael Nguyen <nguyen.michael.26@gmail.com>
2022-08-03 15:05:41 +0000

    update

--- Main.py
+++ Main.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.header("Hello from Snowflake Soldiers!")
 st.image("Michael.jpg", "Michael Nguyen", width = 200)
 st.image("Jackie.jpg", "Jackie Driscoll", width = 200)
-st.image("Sivaji.jpg", "Sivaji", width = 200)
+st.image("Sivaji.jpg", "Sivaji Turimella", width = 200)
