7f79029a830e2097080fdc1009958fe0bc31b231
Michael Nguyen <nguyen.michael.26@gmail.com>
2022-08-08 21:19:59 +0000

    final update

--- pages/Airline Analysis.py
+++ pages/Airline Analysis.py
@@ -29,10 +29,10 @@ def run_query(query):
 
 st.subheader("Total Airline Departure Trends per Day in a Month")
 country = pd.read_sql_query("SELECT DISTINCT(DEPCTRY) from AIRLINE.PUBLIC.OAG_SCHEDULE;", conn)
-choice = st.selectbox("Country", country)
+choice = st.selectbox("Country", country, index = 139)
 
 city = pd.read_sql_query("SELECT DISTINCT(DEPCITY) from AIRLINE.PUBLIC.OAG_SCHEDULE WHERE DEPCTRY = " + "\'" + choice + "\'" + ";", conn)
-cityChoice = st.selectbox("City", city)
+cityChoice = st.selectbox("City", city, index = 197)
 
 data = pd.read_sql_query("SELECT COUNT(DEPCITY) AS CD, FLIGHT_DATE FROM AIRLINE.PUBLIC.OAG_SCHEDULE WHERE DEPCTRY = " + "\'" + choice + "\'" + " AND DEPCITY = " + "\'" + cityChoice + "\'" + " GROUP BY FLIGHT_DATE;", conn)
 
@@ -40,10 +40,13 @@ distance = pd.read_sql_query("SELECT SUM(DISTANCE) AS DISTANCE, FLIGHT_DATE " +
 "FROM AIRLINE.PUBLIC.OAG_SCHEDULE " +
 "WHERE DEPCTRY = " + "\'" + choice + "\'" + " AND DEPCITY = " + "\'" + cityChoice + "\'" + "GROUP BY FLIGHT_DATE;", conn)
 
-minData = pd.read_sql_query("SELECT MIN(CD) as MCD, FLIGHT_DATE FROM " +
-"(SELECT COUNT(DEPCITY)AS CD, FLIGHT_DATE FROM AIRLINE.PUBLIC.OAG_SCHEDULE " +
-"WHERE DEPCTRY = " + "\'" + choice + "\'" + " AND DEPCITY =" + "\'" + cityChoice + "\'" " GROUP BY FLIGHT_DATE) " +
-" WHERE FLIGHT_DATE LIKE \'2023%\' GROUP BY FLIGHT_DATE ORDER BY MCD;", conn)
+# Get Index of the United States
+indexCtry = country[country['DEPCTRY'] == 'US'].index[0]
+indexCity = city[city['DEPCITY'] == 'SNA'].index[0]
+print("\n*********************************")
+print("Country: " + str(indexCtry))
+print("City: "  + str(indexCity))
+print("*********************************\n")
 
 # enumerate returns the key-value pairs for EACH FLIGHT_DATE
 for i, val in enumerate(data['FLIGHT_DATE']):
@@ -51,9 +54,6 @@ for i, val in enumerate(data['FLIGHT_DATE']):
 
 for i, val in enumerate(distance['FLIGHT_DATE']):
     distance['FLIGHT_DATE'].iloc[i] = distance['FLIGHT_DATE'].iloc[i].strftime('%B-%Y')
-
-for i, val in enumerate(minData['FLIGHT_DATE']):
-    minData['FLIGHT_DATE'].iloc[i] =  minData['FLIGHT_DATE'].iloc[i].strftime('%B-%Y')
 
 airlineData = pd.DataFrame({
   'Total Flights for ' + cityChoice: data['CD'],
@@ -65,19 +65,11 @@ distanceData = pd.DataFrame({
   'Flight Date' : distance['FLIGHT_DATE']
 })
 
-minimumData = pd.DataFrame({
-  'Minimum Departures for ' + cityChoice: minData['MCD'],
-  'Flight Date' : minData['FLIGHT_DATE']
-})
 
 airlineData = airlineData.rename(columns={'Flight Date':'index'}).set_index('index')
 distancePlot = distanceData.rename(columns={'Flight Date':'index'}).set_index('index')
-minPlot = minimumData.rename(columns={'Flight Date':'index'}).set_index('index')
 
 st.bar_chart(airlineData, use_container_width=True)
-
-with st.expander("View Minimum Flight Departures"):
-    st.bar_chart(minPlot,  use_container_width=True)
 
 with st.expander("View Total Distance"):
     st.bar_chart(distancePlot,  use_container_width=True)
